17dd7b7582af3cbefd3c7ade2c49d5020f63253c
darkwebtourguide <accounts@chrisadams.io>
2021-07-15 14:59:51 -0400

    desc update

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const program = require('commander')
 const chalk = require('chalk')
 const clipboardy = require('clipboardy')
+const createPassword = require('./func/createPassword')
+const savePassword = require('./func/savePassword')
+
+program.version('1.0.0').description('Crypton Generator')
+
+program
+  .option('-s, --save', 'save password to passwords.txt')
+  .option('-l, --length <number>', 'length of password', '8')
+  .option('-nn, --no-numbers', 'remove numbers')
+  .option('-ns, --no-symbols', 'remove symbols')
+  .parse()
+
+  const { save, length, numbers, symbols } = program.opts()
+
+// Generate password
+const generatedPassword = createPassword(length, numbers, symbols)
+
+// Save and write to file
+if (save) {
+  savePassword(generatedPassword)
+}
+
+// Copy to clipboard
+clipboardy.writeSync(generatedPassword)
+
+// Output generated password
+log(chalk.green('Generated Password: ') + chalk.bold(generatedPassword))
+log(chalk.blue('Password copied to clipboard'))
